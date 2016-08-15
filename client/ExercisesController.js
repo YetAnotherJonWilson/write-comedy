da15cd9779a5c70d25dc2bad462d15fd6560d7cf
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-08-15 02:14:10 -0500

    all save and update buttons seem to be working on exercises page

--- client/ExercisesController.js
+++ client/ExercisesController.js
@@ -2,6 +2,8 @@ angular.module('comedyApp').controller('ExercisesController', ['DataService', '$
     var vm = this;
     var pageData = {};
     var result = '';
+    vm.updatedTextToSave = false;
+
 
     vm.pageData = $localStorage.prevPageData;
     //vm.data = DataService.data;
@@ -11,11 +13,63 @@ angular.module('comedyApp').controller('ExercisesController', ['DataService', '$
 
     vm.getRandomExercise = function(){
         $http.get('/exerciseRandomizer').then(handleSuccess);
-    }
+    };
 
     function handleSuccess(response){
         vm.exerciseName = response.data.exercise;
         vm.exerciseDescription = response.data.description;
+    }
+
+    function handleSavedSuccess(){
+        vm.updatedTextToSave = true;
+    }
+
+    vm.replaceSetup = function(){
+        $http.put('/crud/replacesetup/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess);
+    };
+
+    vm.replacePunch = function(){
+        $http.put('crud/replacepunchline/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addToSetup = function(){
+        $http.put('crud/addtosetup/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addToPunch = function(){
+        $http.put('crud/addtopunchline/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addToSM = function(){
+        $http.put('crud/addtosm/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addToTopic = function(){
+        $http.put('crud/addtotopic/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addToTheme = function(){
+        $http.put('crud/addtotheme/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addAltSetup = function(){
+        $http.put('crud/addaltsetup/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    };
+
+    vm.addAltPunch = function(){
+        $http.put('crud/addaltpunchline/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    }
+
+    vm.addAltSM = function(){
+        $http.put('crud/addaltsm/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    }
+
+    vm.addAltTopic = function(){
+        $http.put('crud/addalttopic/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
+    }
+
+    vm.addAltTheme = function(){
+        $http.put('crud/addalttheme/' + vm.pageData.currentId + '/' + vm.textToUpdate).then(handleSavedSuccess());
     }
 
 }]);
--- server/routes/crud.js
+++ server/routes/crud.js
@@ -142,7 +142,297 @@ router.delete('/deleteitem/:id', function(request, response) {
     });
 });
 
+router.put('/replacesetup/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = request.params.text;
 
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
 
+        client.query('UPDATE setups SET setup=$1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/replacepunchline/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE punchlines SET punchline=$1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addtosetup/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE setups SET setup = setup || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addtopunchline/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE punchlines SET punchline = punchline || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addtoSM/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE subject_matter SET subject_matter = subject_matter || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addtotopic/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE topics SET topic = topic || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addtotheme/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE themes SET theme = theme || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                response.sendStatus(200);
+            }
+        });
+    });
+});
+
+router.put('/addaltsetup/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE alt_setups SET setup = setup || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            }
+            client.query('INSERT INTO alt_setups (setup, title_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM alt_setups WHERE title_id=$2);', [text, id], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    done();
+                } else {
+                response.sendStatus(200);
+            }
+            });
+        });
+    });
+});
+
+router.put('/addaltpunchline/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client, next) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+        client.query('UPDATE alt_punchlines SET punchline = punchline || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            }
+            client.query('INSERT INTO alt_punchlines(punchline, title_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM alt_punchlines WHERE title_id=$2);', [text, id], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    done();
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        });
+    });
+});
+
+router.put('/addaltsm/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE alt_subject_matter SET subject_matter = subject_matter || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            }
+            client.query('INSERT INTO alt_subject_matter(subject_matter, title_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM alt_subject_matter WHERE title_id=$2);', [text, id], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    done();
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        });
+    });
+});
+
+router.put('/addalttopic/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE alt_topics SET topic = topic || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            }
+            client.query('INSERT INTO alt_topics(topic, title_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM alt_topics WHERE title_id=$2);', [text, id], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    done();
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        });
+    });
+});
+
+router.put('/addalttheme/:id/:text', function(request, response) {
+    console.log('request.params:', request.params);
+    var id = request.params.id;
+    var text = ' ' + request.params.text;
+
+    pool.connect(function (err, client) {
+        if (err) {
+            console.log('connection error', err);
+            done();
+        }
+
+        client.query('UPDATE alt_themes SET theme = theme || $1 WHERE title_id=$2;', [text, id], function (err, result) {
+            if (err) {
+                console.log(err);
+                done();
+            }
+            client.query('INSERT INTO alt_themes(theme, title_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM alt_themes WHERE title_id=$2);', [text, id], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    done();
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        });
+    });
+});
 
 module.exports = router;
