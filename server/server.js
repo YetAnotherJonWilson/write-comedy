49c951c3180fae1bb75d39ba33ba5f2c7d2ce337
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-08-22 20:58:07 -0500

    trying connect pg simple

--- server/server.js
+++ server/server.js
@@ -35,11 +35,10 @@ var session = require('express-session');
 
 // See express session docs for information on the options: https://github.com/expressjs/session
 app.use(session({
-    secret: 'maddon',
-    key: 'user',
-    resave: true,
-    saveUninitialized: false,
-    cookie: { maxAge: 1800000, secure: false }
+    store: new (require('connect-pg-simple')(session))(),
+    secret: process.env.FOO_COOKIE_SECRET,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
 }));
 
 app.use(passport.initialize());
