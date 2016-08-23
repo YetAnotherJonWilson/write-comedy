06296abfb767b8f043080745b7efe5750aadba3a
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-08-23 10:16:11 -0500

    set constring = connectionString in server js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ var currentuserjokes = require('./routes/currentuserjokes');
 var crud = require('./routes/crud');
 var pg = require('pg');
 
+// If we are running on Heroku, use the remote database (with SSL)
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL;
+} else {
+    // running locally, use our local database instead
+    connectionString = 'postgres://localhost:5432/ComedyApp';
+}
+
 
 // pg.defaults.ssl = true;
 // pg.connect(process.env.DATABASE_URL || 'postgres://iltehnovjjxpab:_axhzdDANx8JMp1szFZCZu7j9k@ec2-54-243-47-83.compute-1.amazonaws.com:5432/dfhfup9kuh3lcg', function(err, client) {
@@ -36,7 +44,7 @@ var session = require('express-session');
 var pgSession = require('connect-pg-simple')(session);
 app.use(session({
     store: new pgSession({
-        conString : process.env.DATABASE_URL
+        conString : connectionString
     }),
     secret: 'variablecats',
     resave: false,
