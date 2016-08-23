b7a7c87ae66bfe83c5b0dabcf4fa2e81386c1ee9
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-08-22 20:02:35 -0500

    try again

--- server/routes/postnewjoke.js
+++ server/routes/postnewjoke.js
@@ -7,7 +7,7 @@ var parseDbUrl = require("parse-database-url");
 
 // If we are running on Heroku, use the remote database (with SSL)
 if(process.env.DATABASE_URL != undefined) {
-    var config = parseDbUrl(process.env["DATABASE_URL?ssl=true"]);
+    var config = parseDbUrl(process.env["DATABASE_URL"] + "?ssl=true");
 } else {
     // running locally, use our local database instead
     var config = {
