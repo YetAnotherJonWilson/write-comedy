9eb1835e401eb404a5a147ffce62d0f17ff69496
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-10-01 04:20:01 -0500

    refactoring styles, index and Maincontroller for responsive design

--- client/MainController.js
+++ client/MainController.js
@@ -1,6 +1,12 @@
-angular.module('comedyApp').controller('MainController', ['$http', function($http){
+angular.module('comedyApp').controller('MainController', ['$http', '$mdMedia', '$scope', function($http, $mdMedia, $scope){
     var vm = this;
 
     vm.message = "Comedy Write Now";
+
+    $scope.$watch(function() {
+        return $mdMedia('xs') ? 'small' : 'large';
+    }, function(size){
+        vm.screenSize = size;
+    })
 
 }]);
