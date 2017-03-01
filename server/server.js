adbfa9862a99362901f7d727729ab9e73615f63a
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-03-01 01:12:16 -0600

    adding test for randomizer

--- server/server.js
+++ server/server.js
@@ -124,5 +124,7 @@ var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port: ', port);
 });
 
+module.exports = server;
+
 
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
+var chai = require('chai');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var chaiHttp = require('chai-http');
+var server = require('../server/server');
+var should = chai.should();
+var routes = require('../server/routes');
+
+chai.use(chaiHttp);
+
+describe('exercises', function() {
+    it('should send random exercise from /exerciseRandomizer GET', function(done) {
+        chai.request(server)
+            .get('/exerciseRandomizer')
+            .end(function(err, res){
+                res.should.be.an('object');
+                done();
+            });
     });
 });
